96082426ad1a9bc17b59a66d778391f77bcb824f
jjszaniszlo <johnszaniszlo@gmail.com>
2024-09-28 02:10:14 -0700

    instantiate username prompt instead of main menu

--- src/Main.cs
+++ src/Main.cs
@@ -4,15 +4,10 @@ namespace NightmareNegotiations;
 
 public partial class Main : Node3D
 {
-	private PackedScene mainMenuTemplate = GD.Load<PackedScene>("res://scenes/MainMenu/MainMenu.tscn");
+	private PackedScene usernamePromptTemplate = GD.Load<PackedScene>("res://scenes/UsernamePrompt/UsernamePrompt.tscn");
 	
 	public override void _Ready()
 	{
-		AddChild(mainMenuTemplate.Instantiate());
-	}
-
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
-	{
+		AddChild(usernamePromptTemplate.Instantiate());
 	}
 }
