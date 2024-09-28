d0ee76e3e7f28821cc82d332861f3ba117a63bb5
jjszaniszlo <johnszaniszlo@gmail.com>
2024-09-28 04:48:48 -0700

    add comment explanation

--- src/net/User.cs
+++ src/net/User.cs
@@ -3,6 +3,8 @@ using Godot.Collections;
 
 namespace NightmareNegotiations.net;
 
+// This acts as a global state for a WebRTC user.  It also manages the client.
+
 public partial class User : Node
 {
     private Client client;
@@ -42,23 +44,31 @@ public partial class User : Node
                 GD.PrintErr(error);
             }
 
-            // connect SessionDescriptionCreated signal to OnSignalCreated, but pass in connection, so we can
-            // do extra stuff in it.
+            // connect SessionDescriptionCreated signal to OnSignalCreated, but pass in connection as well.
             // this essentially delegates setting the description until it's created by the connection.
             connection.SessionDescriptionCreated += (type, sdp) =>
                 OnSignalSessionCreated(type, sdp, connection);
 
+            // connect IceCandidateCreate signal to OnSignalCandidateCreated, passing in connection, similar to before.
             connection.IceCandidateCreated += (media, index, name) =>
                 OnSignalIceCandidateCreated(media, index, name, connection);
             
+            // add the connection to both the list of connections and the RtcPeer itself.
             connections.Add(peerId, connection);
             rtcPeer.AddPeer(connection, peerId);
         }
 
+        // callbacks for peer connection changes
         rtcPeer.PeerConnected += OnSignalPeerConnected;
         rtcPeer.PeerDisconnected += OnSignalPeerDisconnected;
 
+        // Enable the godot high level multiplayer api, but utilizing WebRTC.
         GetTree().GetMultiplayer().MultiplayerPeer = rtcPeer;
+    }
+
+    public void CreateClient()
+    {
+        client = new Client();
     }
     
     private void OnSignalSessionCreated(string type, string sdp, WebRtcPeerConnection connection)
