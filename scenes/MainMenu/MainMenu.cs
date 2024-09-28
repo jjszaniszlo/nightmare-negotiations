ae03ccac09ba82e99972c673fb5b8d66f6e04196
jjszaniszlo <johnszaniszlo@gmail.com>
2024-09-28 01:10:08 -0700

    Create MainMenu.cs, the script for main menu scene.

---
+++ scenes/MainMenu/MainMenu.cs
@@ -0,0 +1,32 @@
+using Godot;
+
+namespace NightmareNegotiations.scenes.MainMenu;
+
+public partial class MainMenu : Node
+{
+    private PackedScene popUpTemplate = GD.Load<PackedScene>("res://scenes/PopUp/PopUp.tscn");
+    private void OnUserNameEntryProceedPressed()
+    {
+        var usernameText = GetNode<LineEdit>("UsernameEntryBox/Panel/Enter Username").Text;
+        
+        // TODO: Serverside database which keeps track of available usernames.
+
+        if (usernameText.Length == 0 || usernameText.Contains(' '))
+        {
+            var popUp = popUpTemplate.Instantiate<PopUp.PopUp>();
+            popUp.SetMessage("You need to enter a valid username!");
+            AddChild(popUp);
+        }
+        else
+        {
+            GetNode<Control>("UsernameEntryBox").Visible = false;
+            GetNode<Control>("Main Buttons").Visible = true;
+        }
+    }
+
+    private void OnUserNameEntryTextSubmitted(string text)
+    {
+        // submit
+        OnUserNameEntryProceedPressed();
+    }
+}
