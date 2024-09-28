840f718c5a2f9f83ddce47efa492b1d412a7e5ec
jjszaniszlo <johnszaniszlo@gmail.com>
2024-09-28 03:47:17 -0700

    move world and Main to better locations and update names

--- scenes/MainMenu/MainMenu.cs
+++ scenes/MainMenu/MainMenu.cs
@@ -4,11 +4,20 @@ namespace NightmareNegotiations.scenes.MainMenu;
 
 public partial class MainMenu : Node
 {
-    private PackedScene worldTemplate = GD.Load<PackedScene>("res://scenes/world.tscn");
+    private PackedScene worldTemplate = GD.Load<PackedScene>("res://scenes/World/World.tscn");
+
+    private PackedScene lobbyMenuTemplate =
+        GD.Load<PackedScene>("res://scenes/LobbySelectionMenu/LobbySelectionMenu.tscn");
     
     private void OnSinglePlayerButtonPressed()
     {
         GetParent().AddChild(worldTemplate.Instantiate());
+        QueueFree();
+    }
+    
+    private void OnMultiPlayerButtonPressed()
+    {
+        GetParent().AddChild(lobbyMenuTemplate.Instantiate());
         QueueFree();
     }
 }
