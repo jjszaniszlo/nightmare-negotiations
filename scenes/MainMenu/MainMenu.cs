7091f2d8dc209ebb3cd31cc366391b667af8c26d
jjszaniszlo <johnszaniszlo@gmail.com>
2024-09-28 02:09:22 -0700

    remove username entry from main menu

--- scenes/MainMenu/MainMenu.cs
+++ scenes/MainMenu/MainMenu.cs
@@ -4,33 +4,8 @@ namespace NightmareNegotiations.scenes.MainMenu;
 
 public partial class MainMenu : Node
 {
-    private PackedScene popUpTemplate = GD.Load<PackedScene>("res://scenes/PopUp/PopUp.tscn");
     private PackedScene worldTemplate = GD.Load<PackedScene>("res://scenes/world.tscn");
-    private void OnUserNameEntryProceedPressed()
-    {
-        var usernameText = GetNode<LineEdit>("UsernameEntryBox/Panel/Enter Username").Text;
-        
-        // TODO: Serverside database which keeps track of available usernames.
-
-        if (usernameText.Length == 0 || usernameText.Contains(' '))
-        {
-            var popUp = popUpTemplate.Instantiate<PopUp.PopUp>();
-            popUp.SetMessage("You need to enter a valid username!");
-            AddChild(popUp);
-        }
-        else
-        {
-            GetNode<Control>("UsernameEntryBox").Visible = false;
-            GetNode<Control>("Main Buttons").Visible = true;
-        }
-    }
-
-    private void OnUserNameEntryTextSubmitted(string text)
-    {
-        // submit
-        OnUserNameEntryProceedPressed();
-    }
-
+    
     private void OnSinglePlayerButtonPressed()
     {
         GetParent().AddChild(worldTemplate.Instantiate());
