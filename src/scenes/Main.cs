0667f87c2c0bfa4c1bf55c8b3a39c26fbf9c802c
jjszaniszlo <johnszaniszlo@gmail.com>
2024-09-29 13:54:13 -0700

    fix template path and namespace for Main.cs

--- src/scenes/Main.cs
+++ src/scenes/Main.cs
@@ -1,11 +1,11 @@
 using Godot;
 
-namespace NightmareNegotiations.scenes.Main;
+namespace NightmareNegotiations.scenes;
 
 public partial class Main : Node3D
 {
 	public static Globals Globals { get; private set; } = new();
-	private PackedScene usernamePromptTemplate = GD.Load<PackedScene>("res://scenes/UsernamePrompt/UsernamePrompt.tscn");
+	private PackedScene usernamePromptTemplate = GD.Load<PackedScene>("res://Scenes/UsernamePrompt/UsernamePrompt.tscn");
 	
 	public override void _Ready()
 	{
