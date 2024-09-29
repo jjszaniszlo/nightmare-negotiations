42c2ab0e86ad1c8e426e2ff8342e8c2a98563bc9
jjszaniszlo <johnszaniszlo@gmail.com>
2024-09-29 13:50:49 -0700

    add world logic

---
+++ src/scenes/World.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Godot;
+
+namespace NightmareNegotiations.scenes;
+
+public partial class World : Node3D
+{
+    private WorldEnvironment environment;
+    private DirectionalLight3D moon;
+    private DirectionalLight3D sun;
+
+    public override void _Ready()
+    {
+        environment = GetNode<WorldEnvironment>("WorldEnvironment");
+        moon = GetNode<DirectionalLight3D>("Moon");
+        sun = GetNode<DirectionalLight3D>("Sun");
+
+        if (IsInstanceValid(sun))
+        {
+            sun.Position = Vector3.Zero;
+            sun.Rotation = Vector3.Zero;
+            sun.RotationOrder = EulerOrder.Zxy;
+        }
+    }
+
+    public void Update()
+    {
+        
+    }
+}
