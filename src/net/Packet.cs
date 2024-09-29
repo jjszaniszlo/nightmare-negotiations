7c309daecaf82c109e97c80e8050420df2b735b3
jjszaniszlo <johnszaniszlo@gmail.com>
2024-09-29 13:55:37 -0700

    Add BasicPacket, UserInfoPacket and LobbyListReceivePacket

--- src/net/Packet.cs
+++ src/net/Packet.cs
@@ -1,10 +1,37 @@
-﻿using Godot;
+﻿using System.Collections.Generic;
+using Godot;
+using Newtonsoft.Json;
 
 namespace NightmareNegotiations.net;
 
-public partial class Packet : RefCounted
+public partial class BasicPacket : RefCounted
 {
-    public MessageType type;
-    public int id;
-    public string data;
+    [JsonProperty("message")] public Message Message { get; set; }
+    [JsonProperty("id")] public int Id { get; set; }
+}
+
+public partial class UserInfoPacket : BasicPacket
+{
+    [JsonProperty("username")] public string Username { get; set; }
+    
+    public UserInfoPacket()
+    {
+        Message = Message.UserInfo;
+    }
+}
+
+public partial class LobbyListReceivePacket : BasicPacket
+{
+    [JsonProperty("lobby_list")] public List<Lobby> LobbyList { get; set; }
+
+    public LobbyListReceivePacket()
+    {
+        Message = Message.LobbyList;
+    }
+
+    public class Lobby
+    {
+        [JsonProperty("lobby_code")] public string LobbyCode { get; private set; }
+        [JsonProperty("lobby_description")] public string LobbyDescription { get; set; } = "";
+    }
 }
