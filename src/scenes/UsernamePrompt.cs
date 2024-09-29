67511c9dfa790fb1d22f9c0972bcb42f7e2295f0
jjszaniszlo <johnszaniszlo@gmail.com>
2024-09-29 13:56:46 -0700

    Add some basic server connection logic for when the player enters a name

--- src/scenes/UsernamePrompt.cs
+++ src/scenes/UsernamePrompt.cs
@@ -1,13 +1,14 @@
 using Godot;
+using NightmareNegotiations.net;
 
-namespace NightmareNegotiations.scenes.UsernamePrompt;
+namespace NightmareNegotiations.scenes;
 
 public partial class UsernamePrompt : Control
 {
-    private PackedScene popUpTemplate = GD.Load<PackedScene>("res://scenes/PopUp/PopUp.tscn");
-    private PackedScene mainMenuTemplate = GD.Load<PackedScene>("res://scenes/MainMenu/MainMenu.tscn");
+    private PackedScene popUpTemplate = GD.Load<PackedScene>("res://Scenes/PopUp/PopUp.tscn");
+    private PackedScene mainMenuTemplate = GD.Load<PackedScene>("res://Scenes/MainMenu/MainMenu.tscn");
     
-    private void OnUserNameEntryProceedPressed()
+    private async void OnUserNameEntryProceedPressed()
     {
         var usernameText = GetNode<LineEdit>("UsernameEntryBox/Panel/Enter Username").Text;
         
@@ -15,17 +16,32 @@ public partial class UsernamePrompt : Control
 
         if (usernameText.Length == 0 || usernameText.Contains(' '))
         {
-            var popUp = popUpTemplate.Instantiate<PopUp.PopUp>();
+            var popUp = popUpTemplate.Instantiate<PopUp>();
             popUp.SetMessage("You need to enter a valid username!");
             AddChild(popUp);
         }
         else
         {
+            Main.Globals.User.Client = new Client();
+            GetParent().AddChild(Main.Globals.User.Client);
+            
+            await ToSignal(GetTree().CreateTimer(2.0),Timer.SignalName.Timeout);
+
+            if (Main.Globals.User.Client.IsConnectionOpen())
+            {
+                Main.Globals.User.Client.SendUserName(usernameText);
+                GD.Print("[Log] Sending username to server...");
+            }
+            else
+            {
+                GD.Print("[Error] Client not open!");
+            }
+            
             GetParent().AddChild(mainMenuTemplate.Instantiate());
             QueueFree();
         }
     }
-
+    
     private void OnUserNameEntryTextSubmitted(string text)
     {
         // submit
