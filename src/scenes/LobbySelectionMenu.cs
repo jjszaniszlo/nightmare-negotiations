72b23a658abe70d7b377357e119269270c6cea95
jjszaniszlo <johnszaniszlo@gmail.com>
2024-09-29 13:49:56 -0700

    LobbyList querying

--- src/scenes/LobbySelectionMenu.cs
+++ src/scenes/LobbySelectionMenu.cs
@@ -1,10 +1,18 @@
 using Godot;
+using NightmareNegotiations.net;
 
-namespace NightmareNegotiations.scenes.LobbySelectionMenu;
+namespace NightmareNegotiations.scenes;
 
 public partial class LobbySelectionMenu : Control
 {
-    private PackedScene mainMenuTemplate = GD.Load<PackedScene>("res://scenes/MainMenu/MainMenu.tscn");
+    private PackedScene mainMenuTemplate = GD.Load<PackedScene>("res://Scenes/MainMenu/MainMenu.tscn");
+
+    public LobbySelectionMenu()
+    {
+        Main.Globals.User.Client.RequestLobbyList();
+        Main.Globals.User.Client.LobbyListReceived += CreateLobbyList;
+    }
+
     private void OnJoinButtonPressed()
     {
         
@@ -28,5 +36,32 @@ public partial class LobbySelectionMenu : Control
     private void OnRefreshLobbyListButtonPressed()
     {
         
+    }
+
+    private void CreateLobbyList(string[] lobbyCodes, string[] lobbyDescriptions)
+    {
+        if (lobbyCodes.Length == 0 || lobbyDescriptions.Length == 0) return;
+        
+        var vboxContainer = GetNode<VBoxContainer>("PublicLobbyListContainer/LobbyList/Container");
+            
+        // clear scroll container nodes.
+        foreach (var node in vboxContainer.GetChildren()) node?.QueueFree();
+
+        // add all lobbies found to it.
+        for (var i = 0; i < lobbyCodes.Length; i++)
+        {
+            var lobbyEntry = (Control)GetNode<Control>("AvailableLobbyTemplate").Duplicate();
+            lobbyEntry.Show();
+            lobbyEntry.GetChild<Label>(0).Text = lobbyDescriptions[i];
+            lobbyEntry.GetChild<Label>(1).Text = lobbyCodes[i];
+            lobbyEntry.GetChild<Button>(2).Pressed += () =>
+                OnJoinLobbyPressed(lobbyEntry.GetChild<Label>(1).Text);
+            vboxContainer.AddChild(lobbyEntry);
+        }
+    }
+
+    private void OnJoinLobbyPressed(string lobbyCode)
+    {
+        Main.Globals.User.Client.RequestJoinLobby(lobbyCode);
     }
 }
--- src/scenes/MainMenu.cs
+++ src/scenes/MainMenu.cs
@@ -1,14 +1,20 @@
 using Godot;
 
-namespace NightmareNegotiations.scenes.MainMenu;
+namespace NightmareNegotiations.scenes;
 
 public partial class MainMenu : Node
 {
     private PackedScene worldTemplate = GD.Load<PackedScene>("res://Scenes/World/World.tscn");
-
     private PackedScene lobbyMenuTemplate =
-        GD.Load<PackedScene>("res://scenes/LobbySelectionMenu/LobbySelectionMenu.tscn");
-    
+        GD.Load<PackedScene>("res://Scenes/LobbySelectionMenu/LobbySelectionMenu.tscn");
+
+    public override void _Ready()
+    {
+        GetNode<Label>("Connection Status/Online_Offline").Text = Main.Globals.User.Client.IsConnectionOpen()
+            ? "Online"
+            : "Offline";
+    }
+
     private void OnSinglePlayerButtonPressed()
     {
         GetParent().AddChild(worldTemplate.Instantiate());
