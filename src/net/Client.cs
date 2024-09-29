2b45f192598d3f866151d6b101b3886d50f17b58
jjszaniszlo <johnszaniszlo@gmail.com>
2024-09-29 13:53:33 -0700

    Refactor some packet parsing

--- src/net/Client.cs
+++ src/net/Client.cs
@@ -110,75 +110,126 @@ public partial class Client : Node
 		{
 			return;
 		}
-
-		switch (packet.type)
+		if (packet is UserInfoPacket userInfoPacket)
 		{
-			case MessageType.UserInfo:
-				Main.Globals.User.UserName = packet.data;
-				Main.Globals.User.Id = packet.id;
-				
-				GD.Print($"Received User: {packet.data}:{packet.id}");
-				EmitSignal(SignalName.UserNameFeedbackReceived);
-				connected = true;
-				break;
-			case MessageType.LobbyList:
-				if (packet.data == "")
-				{
-					EmitSignal(SignalName.LobbyListReceived, System.Array.Empty<string>());
-				}
-				else
-				{
-					var lobbyList = packet.data.Split("@@@");
-					EmitSignal(SignalName.LobbyListReceived, lobbyList);
-				}
-				break;
-			case MessageType.CreateLobby:
-				if (packet.data == "INVALID")
-				{
-					EmitSignal(SignalName.InvalidNewLobbyName);
-				}
-				else
-				{
-					var splitData = packet.data.Split("@@@", 2);
-					var lobbyCode = splitData[0];
-					var lobbyDescription = splitData[1];
-					EmitSignal(SignalName.NewLobby, lobbyCode, lobbyDescription);
-				}
-				break;
-			case MessageType.JoinLobby:
+			Main.Globals.User.UserName = userInfoPacket.Username;
+			Main.Globals.User.Id = userInfoPacket.Id;
+			
+			GD.Print($"[Log] Received User from Server: {userInfoPacket.Username}:{userInfoPacket.Id}");
+			EmitSignal(SignalName.UserNameFeedbackReceived);
+			connected = true;
+		}
+		else if (packet is LobbyListReceivePacket lobbyListPacket)
+		{
+			if (lobbyListPacket.LobbyList.Count == 0)
 			{
-				var splitData = packet.data.Split("@@@");
-				var header = splitData[0];
-				var payload = splitData[1];
-				if (header == "INVALID")
-				{
-					EmitSignal(SignalName.InvalidJoinLobbyName);
-				}
-				else if (header == "LOBBY_CODE")
-				{
-					EmitSignal(SignalName.JoinLobby, payload);
-				}
-				else if (header == "NEW_PLAYER_JOINED_NAME")
-				{
-					Main.Globals.User.Peers.Add(packet.id, payload);
-					EmitSignal(SignalName.AnotherUserJoinedLobby, payload);
-				}
-				else if (header == "EXISTING_PLAYER_JOINED_NAME")
-				{
-					Main.Globals.User.Peers.Add(packet.id, payload);
-					EmitSignal(SignalName.AnotherUserJoinedLobby, payload);
-				}
-				break;
+				GD.Print("[Log] No available lobbies.");
+				var lobbyCodes = new[]{"XXXAVL", "HJCLAD"};
+				var lobbyDescriptions = new[]{"Cool lobby 1", "Epic Gamers 2"};
+				EmitSignal(SignalName.LobbyListReceived, lobbyCodes, lobbyDescriptions);
 			}
-			case MessageType.LeaveLobby:
-				break;
-			case MessageType.LobbyMessage:
-				break;
-			case MessageType.Offer:
+			else
 			{
-				var splitData = packet.data.Split("@@@", 2);
-				var type = splitData[0];
-				var sdp = splitData[1];
+				var lobbyCodes = lobbyListPacket.LobbyList.Select(lobby => lobby.LobbyCode).ToArray();
+				var lobbyDescriptions = lobbyListPacket.LobbyList.Select(lobby => lobby.LobbyDescription).ToArray();
+				GD.Print($"[Log] {lobbyCodes.Length} available lobbies.");
+				EmitSignal(SignalName.LobbyListReceived, lobbyCodes, lobbyDescriptions);
+			}
+		}
+		// 	case Message.CreateLobby:
+		// 		if (packet.data == "INVALID")
+		// 		{
+		// 			EmitSignal(SignalName.InvalidNewLobbyName);
+		// 		}
+		// 		else
+		// 		{
+		// 			var splitData = packet.data.Split("@@@", 2);
+		// 			var lobbyCode = splitData[0];
+		// 			var lobbyDescription = splitData[1];
+		// 			EmitSignal(SignalName.NewLobby, lobbyCode, lobbyDescription);
+		// 		}
+		// 		break;
+		// 	case Message.JoinLobby:
+		// 	{
+		// 		var splitData = packet.data.Split("@@@");
+		// 		var header = splitData[0];
+		// 		var payload = splitData[1];
+		// 		if (header == "INVALID")
+		// 		{
+		// 			EmitSignal(SignalName.InvalidJoinLobbyName);
+		// 		}
+		// 		else if (header == "LOBBY_CODE")
+		// 		{
+		// 			EmitSignal(SignalName.JoinLobby, payload);
+		// 		}
+		// 		else if (header == "NEW_PLAYER_JOINED_NAME")
+		// 		{
+		// 			Main.Globals.User.Peers.Add(packet.id, payload);
+		// 			EmitSignal(SignalName.AnotherUserJoinedLobby, payload);
+		// 		}
+		// 		else if (header == "EXISTING_PLAYER_JOINED_NAME")
+		// 		{
+		// 			Main.Globals.User.Peers.Add(packet.id, payload);
+		// 			EmitSignal(SignalName.AnotherUserJoinedLobby, payload);
+		// 		}
+		// 		break;
+		// 	}
+		// 	case Message.LeaveLobby:
+		// 		break;
+		// 	case Message.LobbyMessage:
+		// 		break;
+		// 	case Message.Offer:
+		// 	{
+		// 		var splitData = packet.data.Split("@@@", 2);
+		// 		var type = splitData[0];
+		// 		var sdp = splitData[1];
+		//
+		// 		EmitSignal(SignalName.RtcOfferReceived, type, sdp, packet.id);
+		// 		break;
+		// 	}
+		// 	case Message.Answer:
+		// 	{
+		// 		var splitData = packet.data.Split("@@@", 2);
+		// 		var type = splitData[0];
+		// 		var sdp = splitData[1];
+		//
+		// 		EmitSignal(SignalName.RtcAnswerReceived, type, sdp, packet.id);
+		// 		break;
+		// 	}
+		// 	case Message.InteractiveConnectivityEstablishment:
+		// 	{
+		// 		var splitData = packet.data.Split("@@@", 3);
+		// 		var media = splitData[0];
+		// 		var index = splitData[1].ToInt();
+		// 		var name = splitData[2];
+		//
+		// 		EmitSignal(SignalName.RtcIceReceived, media, index, name, packet.id);
+		// 		break;
+		// 	}
+		// 	case Message.StartSession:
+		// 		break;
+		// 	case Message.Host:
+		// 		// assign User to host if they are hosting the lobby, otherwise assign their HostName property to the current
+		// 		// host of the lobby.
+		// 		if (packet.id == Main.Globals.User.Id && packet.data == Main.Globals.User.UserName)
+		// 		{
+		// 			Main.Globals.User.IsHost = true;
+		// 		}
+		// 		else
+		// 		{
+		// 			Main.Globals.User.IsHost = false;
+		// 			Main.Globals.User.HostUserName = packet.data;
+		// 		}
+		//
+		// 		EmitSignal(SignalName.ServerChangedHost);
+		// 		break;
+		// }
+	}
+
+	private void SendPacket(BasicPacket packet)
+	{
+		wsPeer.SendText(JsonConvert.SerializeObject(packet));
+	}
 
 				EmitSignal(SignalName.RtcOfferReceived, type, sdp, packet.id);
 				break;
