839a88d3ea87bf88455cc7eb6a06120b874f493a
jjszaniszlo <johnszaniszlo@gmail.com>
2024-09-30 12:54:23 -0700

    Initialize either Client or Server depending on os feature "dedicated_server"

--- src/scenes/Main.cs
+++ src/scenes/Main.cs
@@ -1,13 +1,23 @@
 using Godot;
+using NightmareNegotiations.net;
 
 namespace NightmareNegotiations.scenes;
 
 public partial class Main : Node3D
 {
-	private PackedScene usernamePromptTemplate = GD.Load<PackedScene>("res://scenes/UsernamePrompt/UsernamePrompt.tscn");
+	private PackedScene usernamePromptTemplate;
 	
 	public override void _Ready()
 	{
-		AddChild(usernamePromptTemplate.Instantiate());
+		if (OS.HasFeature("dedicated_server"))
+		{
+			NetworkManager.Instance.InitDedicatedServer();
+		}
+		else
+		{
+			NetworkManager.Instance.InitClient();
+		}
+		
+		usernamePromptTemplate = GD.Load<PackedScene>("res://Scenes/UsernamePrompt/UsernamePrompt.tscn");
 	}
 }
