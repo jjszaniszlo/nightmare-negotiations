12fd3265ac23def53ddb7bc45b2addee3be6af88
kinsch <maquenzie-noele.garcia.950@my.csun.edu>
2024-10-22 02:40:57 -0700

    Begin working on multiplayer chat, allow it to work on computer locally
    Current bug: Has the messages appear in the wrong text box
    Note: For testing, must run project with 2 instances

---
+++ Scenes/MultiplayerChat/multiplayer_chat.gd
@@ -0,0 +1,44 @@
+extends Node2D
+
+@onready var host = $Host
+@onready var join = $Join
+@onready var username = $Username
+@onready var send = $Send
+@onready var message = $Message
+@onready var messages = $Message
+
+var usernm : String #Username
+var msg : String #Msg 
+
+
+func _on_host_pressed() -> void:
+	var peer = ENetMultiplayerPeer.new() 
+	peer.create_server(1027) #Make new server, currently has port 1027
+	get_tree().set_multiplayer(SceneMultiplayer.new(),self.get_path())
+	multiplayer.multiplayer_peer = peer
+	joined()
+	
+
+
+func _on_join_pressed() -> void:
+	var peer = ENetMultiplayerPeer.new()
+	peer.create_client("127.0.0.1",1027) #Reference a different IP when forwarding to a server 
+	get_tree().set_multiplayer(SceneMultiplayer.new(),self.get_path())
+	multiplayer.multiplayer_peer = peer
+	joined()
+
+func _on_send_pressed() -> void:
+	rpc("msg_rpc", usernm, message.text)
+	message.text = "" #Clear text after every message, reset to blank string
+
+@rpc ("any_peer", "call_local")
+
+func msg_rpc(usernm, data):
+	messages.text += str(usernm, ": ", data, "\n")
+	#messages.scroll_vertical = INF
+
+func joined():
+	host.hide()
+	join.hide()
+	username.hide()
+	usernm = username.text 
