7a7fecbe4f35f07b80bd18f12393f2a57c55aae1
jjszaniszlo <johnszaniszlo@gmail.com>
2024-10-22 02:24:00 -0700

    add TerrainGeneration.cs

--- Source Code/Terrain/TerrainGeneration.cs
+++ Source Code/Terrain/TerrainGeneration.cs
@@ -2,34 +2,16 @@ using Godot;
 
 namespace NightmareNegotiations.Terrain;
 
-public partial class TerrainGeneration : Node3D
+[Tool]
+public partial class TerrainGeneration : StaticBody3D
 {
-	[Export]
-	private MeshInstance3D terrainMesh;
-
-	[Export] private int sizeDepth = 100;
-	[Export] private int sizeWidth = 100;
-	[Export] private int meshResolution = 1;
-	
-	[Export] private FastNoiseLite noise;
-	
-	[Export] private Material material;
-	
-	public override void _Ready()
-	{
-		GenerateTerrain();	
-	}
-
-	private void GenerateTerrain()
-	{
-		var planeMesh = new PlaneMesh();
-		planeMesh.Size = new Vector2(sizeWidth, sizeDepth);
-		planeMesh.SubdivideDepth = sizeDepth * meshResolution - 1;
-		planeMesh.SubdivideWidth = sizeWidth * meshResolution - 1;
-		planeMesh.Material = material;
-	}
-
-	public override void _Process(double delta)
-	{
-	}
+    public void GenerateMesh()
+    {
+        var planeMesh = new PlaneMesh();
+        planeMesh.Size = new Vector2(10, 10);
+        planeMesh.SubdivideWidth = 9;
+        planeMesh.SubdivideDepth = 9;
+        
+        
+    }
 }
