be4aaf295c53d5d40070c573298f8f9cd4e6cd1b
jjszaniszlo <johnszaniszlo@gmail.com>
2024-10-21 22:39:59 -0700

    alter event name

--- Source Code/Utils/StateMachine.cs
+++ Source Code/Utils/StateMachine.cs
@@ -16,12 +16,12 @@ public partial class StateMachine : RefCounted
         {
             EmitSignal(SignalName.OnLeaveCurrentState, currentState);
             currentState = value;
-            EmitSignal(SignalName.OnNewState, currentState);
+            EmitSignal(SignalName.OnEnterState, currentState);
         }
     }
 
     [Signal] public delegate void OnTransitionEventHandler();
-    [Signal] public delegate void OnNewStateEventHandler(long newState);
+    [Signal] public delegate void OnEnterStateEventHandler(long newState);
     [Signal] public delegate void OnLeaveCurrentStateEventHandler(long leftState);
 
     public void AddState(long stateId, string stateName)
