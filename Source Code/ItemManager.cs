4343c64b966b644e85e2a4b936c3d6a27950481c
jjszaniszlo <johnszaniszlo@gmail.com>
2024-10-27 20:34:02 -0700

    - add basic Lobby - add computer scene and computer interface

--- Source Code/ItemManager.cs
+++ Source Code/ItemManager.cs
@@ -13,9 +13,9 @@ public partial class ItemManager : Node3D
 
 	public override void _Ready()
 	{
-		var flashlight = Globals.Instance.ItemManifest["flashlight"].Instantiate<Flashlight>();
-		flashlight.Enable(this);
-		ItemPivot.AddChild(flashlight);
+		// var flashlight = Globals.Instance.ItemManifest["flashlight"].Instantiate<Flashlight>();
+		// flashlight.Enable(this);
+		// ItemPivot.AddChild(flashlight);
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -23,20 +23,7 @@ public partial class ItemManager : Node3D
 	{
 		if (Input.IsActionJustPressed("itemUse"))
 		{
-			GD.Print("Item used!");
 			EmitSignal(SignalName.ItemUsed);
 		}
-	}
-
-	public override void _Input(InputEvent @event)
-	{
-		// if (@event is InputEventAction action)
-		// {
-		// 	if (action.Action == "itemUse" && action.Pressed)
-		// 	{
-		// 		GD.Print("Item used!");
-		// 		EmitSignal(SignalName.ItemUsed);
-		// 	}
-		// }
 	}
 }
