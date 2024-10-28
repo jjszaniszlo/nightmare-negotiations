6ae6754410f3534f7225780590e60afe74f77a2f
jjszaniszlo <johnszaniszlo@gmail.com>
2024-10-27 21:55:36 -0700

    add computer Interactable

---
+++ Source Code/Interactable/ComputerInteractable.cs
@@ -0,0 +1,11 @@
+﻿using Godot;
+
+namespace NightmareNegotiations.Interactable;
+
+public partial class ComputerInteractable : Interactable
+{
+    public override void Interact()
+    {
+        EmitSignal(Interactable.SignalName.OnInteract);
+    }
+}
