71a75261a800ef7b8932aaff8210464865b2ee9f
jjszaniszlo <johnszaniszlo@gmail.com>
2024-10-27 19:06:55 -0700

    fix flashlight

---
+++ Source Code/Items/Flashlight.cs
@@ -0,0 +1,33 @@
+using Godot;
+
+namespace NightmareNegotiations.Items;
+
+public partial class Flashlight : Node3D, IItem
+{
+	[Export] public SpotLight3D Light { get; private set; }
+	public override void _Process(double delta)
+	{
+	}
+
+	public override void _Ready()
+	{
+		Light.LightEnergy = 0.0f;
+	}
+	
+	public void Enable(ItemManager itemManager)
+	{
+		SetProcess(true);
+		itemManager.ItemUsed += OnUse;
+	}
+
+	public void Disable(ItemManager itemManager)
+	{
+		SetProcess(false);
+		itemManager.ItemUsed -= OnUse;
+	}
+
+	public void OnUse()
+	{
+		Light.LightEnergy = Light.LightEnergy == 0.0f ? 4.0f : 0.0f;
+	}
+}
