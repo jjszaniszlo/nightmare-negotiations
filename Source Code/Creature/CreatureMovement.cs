9015518d9f4aebcf9d7d56da3c115e6941bec36f
jjszaniszlo <johnszaniszlo@gmail.com>
2024-10-28 10:40:15 -0700

    - import assets - create blend tree - create base movement class - create base behavior class

---
+++ Source Code/Creature/CreatureMovement.cs
@@ -0,0 +1,18 @@
+using Godot;
+using System;
+
+public partial class CreatureMovement : CharacterBody3D
+{
+	public override void _PhysicsProcess(double delta)
+	{
+		Vector3 velocity = Velocity;
+
+		if (!IsOnFloor())
+		{
+			velocity += GetGravity() * (float)delta;
+		}
+		
+		Velocity = velocity;
+		MoveAndSlide();
+	}
+}
